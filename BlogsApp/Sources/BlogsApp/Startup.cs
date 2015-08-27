a4bca3b65fe423a9b4b5e9987661f5e39580400e
Nerielle <nelya.iva@gmail.com>
2015-08-27 10:43:15 +0300

    + Mvc Project

---
+++ BlogsApp/Sources/BlogsApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BlogsApp.Startup))]
+namespace BlogsApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
