9fdbcfee20888273c52bcf4e426d3f7bea9b5963
Nerielle <nelya.iva@gmail.com>
2015-08-29 23:20:23 +0300

    Paging

--- Sandbox/Sources/Sandbox/Models/QueryOptions.cs
+++ Sandbox/Sources/Sandbox/Models/QueryOptions.cs
@@ -11,7 +11,14 @@ namespace Sandbox.Models
         {
             SortField = "Id";
             SortOrder = SortOrder.Asc;
+            CurrentPage = 1;
+            PageSize = 1;
         }
+        public int CurrentPage { get; set; }
+
+        public int TotalPages { get; set; }
+
+        public int PageSize { get; set; }
         public string SortField { get; set; }
         public SortOrder SortOrder { get; set; }
         public string Sort { get
