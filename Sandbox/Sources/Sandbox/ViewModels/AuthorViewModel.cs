98f8554e821e91912cfa64036eaed72141d06cf7
Nerielle <nelya.iva@gmail.com>
2015-09-13 22:13:34 +0300

    +AuthorViewModel

---
+++ Sandbox/Sources/Sandbox/ViewModels/AuthorViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sandbox.ViewModels
+{
+    public class AuthorViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        public string Biography { get; set; }
+    }
+}
