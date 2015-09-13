b982c29de4f4e30c423b530af2396bfd07a7ef6c
Nerielle <nelya.iva@gmail.com>
2015-09-13 19:25:07 +0300

    modal dialog for books deleting action

---
+++ Sandbox/Sources/Sandbox/Scripts/ViewModels/BookIndexViewModel.js
@@ -0,0 +1,16 @@
+﻿function BookIndexViewModel(books) {
+    var self = this;
+    self.books = books;
+    self.showDeleteModal = function (data, event) {
+        self.sending = ko.observable(false);
+        $.get($(event.target).attr('href'), function (d) {
+            $('.body-content').prepend(d);
+            $('#deleteModal').modal('show');
+            ko.applyBindings(self, document.getElementById('deleteModal'));
+        });
+    };
+    self.deleteBook = function (form) {
+        self.sending(true);
+        return true;
+    };
+};
