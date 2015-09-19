8b7c514fe32418faf07fb21ffa00677e51f316ad
Nerielle <nelya.iva@gmail.com>
2015-09-19 20:01:08 +0300

    + web api 2 controller

--- Sandbox/Sources/Sandbox/Global.asax.cs
+++ Sandbox/Sources/Sandbox/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Web.Optimization;
 using System.Web.Routing;
 using Sandbox.DAL;
 using System.Data.Entity;
+using System.Web.Http;
 
 namespace Sandbox
 {
@@ -11,6 +12,7 @@ namespace Sandbox
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
