f75532456ac0413aff86c8bf8ef53186d9c6758a
Nerielle <nelya.iva@gmail.com>
2015-10-03 19:22:05 +0300

    + nhibernate mappings

---
+++ Blog/Dal/Mapping/ArticleMap.cs
@@ -0,0 +1,21 @@
+﻿using NHibernate.Mapping.ByCode.Conformist;
+
+namespace Dal.Mapping
+{
+    public class ArticleMap:ClassMapping<Article>
+    {
+        public ArticleMap()
+        {
+            Table("Article");
+            Id(x=>x.Id);
+            Property(x=>x.Content);
+            Property(x=>x.Date);
+            Property(x=>x.Description, map=>map.NotNullable(false));
+            Property(x=>x.Title);
+            Bag(x=>x.Comments, map =>
+            {
+                map.Key(x=>x.Column("ArticleId"));
+            });
+        }
+    }
+}
---
+++ Blog/Dal/Mapping/CommentMap.cs
@@ -0,0 +1,16 @@
+﻿using NHibernate.Mapping.ByCode.Conformist;
+
+namespace Dal.Mapping
+{
+    public class CommentMap : ClassMapping<Comment>
+    {
+        public CommentMap()
+        {
+            Table("Comment");
+            Id(x => x.Id);
+            Property(x => x.Content);
+            Property(x => x.Date);
+            ManyToOne(x => x.Article);
+        }
+    }
+}
