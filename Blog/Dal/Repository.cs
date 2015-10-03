b534d6d5969c16cf8c806560a85f74d13ffcca3c
Nerielle <nelya.iva@gmail.com>
2015-10-03 22:06:44 +0300

    + unitofwork

---
+++ Blog/Dal/Repository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Dal.Mapping;
+
+namespace Dal
+{
+    public class Repository:IDisposable
+    {
+        private readonly UnitOfWork unitOfWork;
+        public Repository()
+        {
+            this.unitOfWork = new UnitOfWork();
+        }
+
+        public IList<Article> GetArticles()
+        {
+            return unitOfWork.Query<Article>().ToList();
+        }
+
+        public void Dispose()
+        {
+            unitOfWork.Dispose();
+        }
+    }
+}
---
+++ Blog/Dal/UnitOfWork.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using Dal.Mapping;
+using NHibernate;
+using NHibernate.Linq;
+using NHibernate.Persister.Entity;
+
+namespace Dal
+{
+    public class UnitOfWork:IDisposable
+    {
+        private ISession session;
+        private ITransaction transacion;
+        private bool isCommited;
+        public UnitOfWork()
+        {
+            session = SessionFactory.OpenSession();
+            transacion = session.BeginTransaction();
+        }
+
+        public void Save(DomainObject domainObject)
+        {
+            session.SaveOrUpdate(domainObject);
+        }
+        public void Commit()
+        {
+            transacion.Commit();
+            isCommited = true;
+        }
+
+        public void Dispose()
+        {
+            if (!isCommited)
+            {
+                transacion.Commit();
+            }
+            session.Dispose();
+        }
+
+        public DomainObject GetById<T>(Guid id) where T: DomainObject
+        {
+            return session.Get<T>(id);
+        }
+
+        public IQueryable<T> Query<T>()
+        {
+            return session.Query<T>();
+        }
+    }
+}
