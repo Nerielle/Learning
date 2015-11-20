4804f1dc49a8ad5167fcf06c0b3c480cf61769fd
Nerielle <nelya.iva@gmail.com>
2015-11-20 09:54:27 +0300

    +models

---
+++ Sandbox/Shop/DAL/ShopDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using Shop.Models;
+
+namespace Shop.DAL
+{
+    public class ShopDbContext: DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Sandbox/Shop/Models/Cart.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Shop.Models
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+
+        [Index(IsUnique = true)]
+        [StringLength(255)]
+        public string SessionId { get; set; }
+
+        public virtual ICollection<CartItem> CartItems { get; set; }
+    }
+}
