ac2a1eaff900ec910af45f6cea2edf33d44c9963
Nerielle <nelya.iva@gmail.com>
2015-11-20 10:45:00 +0300

    +CategoriesController

---
+++ Sandbox/Shop/Controllers/CategoriesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Shop.Models;
+using Shop.Services;
+using Shop.ViewModels;
+
+namespace Shop.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly CategoryService _categoryService = new CategoryService();
+
+        [ChildActionOnly]
+        public PartialViewResult Menu(int selectedCategoryId)
+        {
+            var categories = _categoryService.Get();
+
+            AutoMapper.Mapper.CreateMap<Category, CategoryViewModel>();
+
+            ViewBag.SelectedCategoryId = selectedCategoryId;
+
+            return PartialView(
+              AutoMapper.Mapper.Map<List<Category>, List<CategoryViewModel>>(categories)
+            );
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _categoryService.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Sandbox/Shop/Services/CategoryService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Shop.DAL;
+using Shop.Models;
+
+namespace Shop.Services
+{
+    public class CategoryService : IDisposable
+    {
+        private readonly ShopDbContext _db = new ShopDbContext();
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public List<Category> Get()
+        {
+            return _db.Categories.OrderBy(c => c.Name).ToList();
+        }
+    }
+}
