351b6fdf0c272af8bf3195669fb75fce5fc696cc
Nerielle <nelya.iva@gmail.com>
2015-11-20 14:21:26 +0300

    +BooksController

---
+++ Sandbox/Shop/Controllers/BooksController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using AutoMapper;
+using Shop.Models;
+using Shop.Services;
+using Shop.ViewModels;
+
+namespace Shop.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly BookService _bookService = new BookService();
+
+        public BooksController()
+        {
+            Mapper.CreateMap<Book, BookViewModel>();
+            Mapper.CreateMap<Author, AuthorViewModel>();
+            Mapper.CreateMap<Category, CategoryViewModel>();
+        }
+
+        [ChildActionOnly]
+        public PartialViewResult Featured()
+        {
+            var books = _bookService.GetFeatured();
+
+            return PartialView(
+                Mapper.Map<List<Book>, List<BookViewModel>>(books)
+                );
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _bookService.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Sandbox/Shop/Services/BookService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Shop.DAL;
+using Shop.Models;
+
+namespace Shop.Services
+{
+    public class BookService
+    {
+        private readonly ShopDbContext _db = new ShopDbContext();
+
+        public List<Book> GetFeatured()
+        {
+            return _db.Books.
+                Include("Author").
+                Where(b => b.Featured).
+                ToList();
+        }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+    }
+}
