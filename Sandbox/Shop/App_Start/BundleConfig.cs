78caaa02ecd24ff3e33919ce238eb1b60422bb5c
Nerielle <nelya.iva@gmail.com>
2015-11-21 20:11:26 +0300

    errors  fixed: cartDetails

--- Sandbox/Shop/App_Start/BundleConfig.cs
+++ Sandbox/Shop/App_Start/BundleConfig.cs
@@ -14,7 +14,10 @@ namespace Shop
             "~/Scripts/respond.js", "~/Scripts/knockout-{version}.js",
             "~/Scripts/custom.js",
             "~/Scripts/viewModels/cart-summary-viewmodel.js"));
-
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
             bundles.Add(new StyleBundle("~/Content/css").Include(
                   "~/Content/bootstrap.css",
                   "~/Content/site.css"));
