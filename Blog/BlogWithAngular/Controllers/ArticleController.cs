04a357c6afb75ebeecd42f63939cb050e0c36c7a
Nerielle <nelya.iva@gmail.com>
2015-12-23 18:13:43 +0300

    articles web api controller

---
+++ Blog/BlogWithAngular/Controllers/ArticleController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using Dal;
+
+namespace BlogWithAngular.Controllers
+{
+    public class ArticleController : ApiController
+    {
+        private readonly Repository repository = new Repository();
+        // DELETE: api/Article/5
+        public void Delete(int id)
+        {
+        }
+
+        // GET: api/Article
+        public IEnumerable<Article> Get()
+        {
+            return repository.GetArticles();
+        }
+
+        // GET: api/Article/5
+        public Article Get(Guid id)
+        {
+            return repository.GetById<Article>(id);
+        }
+
+        // POST: api/Article
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Article/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+    }
+}
