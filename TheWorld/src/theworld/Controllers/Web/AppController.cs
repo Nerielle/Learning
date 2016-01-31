78760b72bd7ce440970d737856ae097b6f3b14e2
Nerielle <nelya.iva@gmail.com>
2016-01-31 23:33:15 +0300

    +asp.net 5 test app

---
+++ TheWorld/src/theworld/Controllers/Web/AppController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace theworld.Controllers.Web
+{
+    public class AppController: Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TheWorld/src/theworld/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿//site.js
+(function() {
+    var $sidebarAndWrapper = $("#sidebar, #wrapper");
+    $("#sidebarToggle").click(function() {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide Sidebar");
+        }
+    });
+
+})();
+
+
