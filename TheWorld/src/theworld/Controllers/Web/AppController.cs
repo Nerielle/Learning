f5651b8e8bf5cf32832a556249dca236a1b20ac1
Nerielle <nelya.iva@gmail.com>
2016-02-11 22:23:42 +0300

    mvc 6

--- TheWorld/src/theworld/Controllers/Web/AppController.cs
+++ TheWorld/src/theworld/Controllers/Web/AppController.cs
@@ -1,11 +1,54 @@
 ﻿using Microsoft.AspNet.Mvc;
+using theworld.Services;
+using theworld.ViewModels;
 
 namespace theworld.Controllers.Web
 {
-    public class AppController: Controller
+    public class AppController : Controller
     {
+        private readonly IMailService mailService;
+
+        public AppController(IMailService service)
+        {
+            mailService = service;
+        }
+
         public IActionResult Index()
         {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var email = Startup.Configuration["AppSettings:SiteEmailAddress"];
+                if (string.IsNullOrWhiteSpace(email))
+                {
+                    ModelState.AddModelError("", "Could not send email");
+                }
+                if (
+                    mailService.SendMail(email,
+                        "",
+                        $"Contact Page from {model.Name} ({model.Email})",
+                        model.Message))
+                {
+                    ModelState.Clear();
+                    ViewBag.Message = "Mail sent. Thanks";
+                }
+            }
+
             return View();
         }
     }
---
+++ TheWorld/src/theworld/Services/DebugMailService.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+namespace theworld.Services
+{
+    public class DebugMailService:IMailService
+    {
+        public bool SendMail(string to, string @from, string subject, string body)
+        {
+            Debug.WriteLine($"Send mail: To: {to}, Subject: {subject}");
+            return true;
+        }
+    }
+}
