e3266f39be5ddff6ea5f79218e15319093b223a8
Nerielle <nelya.iva@gmail.com>
2016-03-26 19:13:12 +0300

    saving new stops

--- TheWorld/theworld/wwwroot/js/tripEditorController.js
+++ TheWorld/theworld/wwwroot/js/tripEditorController.js
@@ -10,17 +10,33 @@
         vm.errorMessage = "";
         vm.newStop = {};
         vm.isBusy = true;
-        $http.get("/api/trips/" + vm.tripName + "/stops")
+        var url = "/api/trips/" + vm.tripName + "/stops";
+        $http.get(url)
             .then(function(response) {
                 angular.copy(response.data, vm.stops);
                 showMap(vm.stops);
-                vm.errorMessage = "";
             }, function(error) {
                 vm.errorMessage = "Failed to load stops";
             })
             .finally(function() {
                 vm.isBusy = false;
             });
+
+        vm.addStop = function() {
+            vm.isBusy = true;
+            $http.post(url, vm.newStop)
+            .then(function (response) {
+                vm.stops.push(response.data);
+                showMap(vm.stops);
+                    vm.newStop = {};
+                },function(error) {
+                    vm.errorMessage = "Failed to add new stop";
+                })
+              .finally(function () {
+                  vm.isBusy = false;
+              });
+
+        };
     }
 
     function showMap(stops) {
